848cca00329daa8b00ab1379b12d1563736632f3
Danilll1 <gabrick_72@mail.ru>
2025-06-18 11:59:53 +0500

    добавили public_html

--- config/filesystems.php
+++ config/filesystems.php
@@ -72,6 +72,8 @@ return [
     'links' => [
         public_path('storage') => storage_path('app/public'),
         public_path('images') => storage_path('app/images'),
+
+        public_path('public_html') => storage_path('public'),
     ],
 
 ];
